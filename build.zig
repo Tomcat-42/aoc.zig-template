a360d5a0cb5cb3eabf624b67c5464d32558ecfd1
Pablo Alessandro Santos Hugen <pablo.hugen@bry.com.br>
2024-12-03 09:37:36 -0300

    Build: Change test dependency graph

--- build.zig
+++ build.zig
@@ -148,17 +148,21 @@ pub fn build(b: *Build) !void {
         .target = target,
         .optimize = optimize,
     });
-    const run_lib_unit_tests = b.addRunArtifact(problem_unit_tests);
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    const run_lib_unit_tests = b.addRunArtifact(problem_unit_tests);
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
+
+    problem_unit_tests.step.dependOn(setup_step);
+    exe_unit_tests.step.dependOn(setup_step);
+
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
-    test_step.dependOn(setup_step);
 
     // clean
     const clean_step = b.step("clean", "Remove build artifacts");
